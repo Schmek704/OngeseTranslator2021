bd03f185df09e00d663b7fd69085e3515877221f
Schmek704 <dmckoenvol@gmail.com>
2021-12-16 14:40:23 -0500

    Initial commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+# This app will take inout from the user and translate the word or phrase into
+# a new language called Ongese.
---
+++ main.py
@@ -0,0 +1,24 @@
+# Ongese Translator
+
+import streamlit as st
+
+# Defining the Function
+def trans(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "bcdfghjklmnpqrstvwxyz":  # Checking for consonants to add "ong" to the end
+            translation = translation + letter + "ong"
+        else:
+            translation = translation + letter + " "  # if not a consonant then letter in the word remains unchanged
+    return translation
+# Asking the User for Input
+
+# Introducing the user to the newest language in town!
+st.title("Ongese Language Translator")
+st.write("This function is made available publicly by Dan and E McKeon")
+
+word = st.text_input("\n\nPlease enter a word or phrase:  ")
+if word:
+    st.write("\nWelcome to the world of Ongese!!!")
+    st.write("\nThe translation of your phrase in Ongese is:")
+    st.write("\n\n" + trans(word))
